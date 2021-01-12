67a08b8ff587b52316b1baed087f0c887ac41f67
gabrielvell <74545050+gabrielvell@users.noreply.github.com>
2021-01-12 16:38:09 +0100

    Fixed errors

--- Assignment/Assets/Player/Wick.cs
+++ Assignment/Assets/Player/Wick.cs
@@ -9,8 +9,6 @@ public class Wick : MonoBehaviour
     private float _speed = 3.5f;
     private float _force = 1000f;
 
-    private bool gameWon = false;
-    private bool levelStarted = false;
 
     void /// <summary>
     /// Start is called on the frame when a script is enabled just before
@@ -18,46 +16,26 @@ public class Wick : MonoBehaviour
     /// </summary>
         Start()
         {
-        transform.position = new Vector3(0,0,0);         
+        transform.position = new Vector3(0,0,0);
         }
     void Update() 
     {
-        CheckPlayAgain();
-    void CheckPlayAgain()
-    {
-        if (gameWon == true)
-        {
-            if (Input.GetKeyDown(KeyCode.Return))
             {
-                gameWon = false;
-            }
-        }
-    }
-    { 
-        // if game is not won yet, allow movement
-        if (!gameWon)
-        {
-            // if level hasn't started, don't check movement updates
-            // only time that allows any movement (intro sound and level start sound done playing)
-            if (levelStarted == true)
-            {
-                // Stores current position of GameObject
-                Vector2 pos = transform.localPosition; // Get current position of transform, everytime update position, need to know previous position 
         //move the player to the right
         //realtime
         //x * realtime(Time.deltaTime) = 1m/s
         //1 * fps e.g 1 * 1500fps (this calculation is used when not multiplying by Time.deltaTime)
         
         //if we press the right arrow move the player to the right
-        if(Input.GetKey(KeyCode.RightArrow))
+        if(Input.GetKeyDown(KeyCode.RightArrow))
         {
             //move the player to the right
-            transform.Translate(_speed * Time.deltaTime,0,0);
+            transform.position += Vector3.right;
         } 
-        else if(Input.GetKey(KeyCode.LeftArrow))
+        else if(Input.GetKeyDown(KeyCode.LeftArrow))
         {
             //move the player to the left
-            transform.Translate(-_speed * Time.deltaTime,0,0);
+            transform.position += Vector3.left;
         }
         if(Input.GetKeyDown(KeyCode.UpArrow))
         {
@@ -67,20 +45,7 @@ public class Wick : MonoBehaviour
         {
             transform.position += Vector3.down;
         }
-        //if the up arrow is pressed then component should jump
-        //bool pressedUpArrow = Input.GetButtonDown("Jump");
-        //if uprrow is pressed pressedUpArrow = true
-        //if(pressedUpArrow)
-        //{
-            //storing rigidbody2d so component can jump when obsticles are shown
-        //    Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
-            //add upwards force to rigidbody
-            //Vector3(0,1,0) * force(1000f)
-        //    rigidbody.AddForce(Vector3.up * _force);
-        //}
             }
         }
     }
-    }
-}
 
